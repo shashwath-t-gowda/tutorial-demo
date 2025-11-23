aed7f6c14d86473e1190eac1803b3465b8f31655
shashwath-t-gowda <shashwath.stg@gmail.com>
2025-11-23 13:09:46 +0530

    Add new python file and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is my first Git Repositories 
 <br>
 Author - Shashwath T Gowda
+<br>
+This is a change, does it look good.
---
+++ main.py
@@ -0,0 +1,6 @@
+# This is main file for tutorial-demo package
+
+print("Hello World!")
+
+for i in range(5):
+    print(f"Iteration {i + 1}")
